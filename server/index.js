4673a5f9ea77ec4087598c40489c179a2979b49c
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-04 19:21:38 -0300

    feat: db connection

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
+const connectDB = require("./config/db");
 const schema = require("./schema/schema");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 app.use(
   "/graphql",
