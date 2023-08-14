3c84c95e65c6a676347cb12c47c28654f1d0a4cd
Angelo Fernandes Machado Fonseca <angelofmfonseca@gmail.com>
2023-08-14 19:00:41 -0300

    feat: ClientRow fron updateCache to refetchQueries

--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -2,19 +2,12 @@ import { FaTrash } from "react-icons/fa";
 import { useMutation } from "@apollo/client";
 import { DELETE_CLIENT } from "../mutations/client";
 import { GET_CLIENTS } from "../queries/client";
+import { GET_PROJECTS } from "../queries/project";
 
 const ClientRow = ({ client }) => {
   const [deleteClient] = useMutation(DELETE_CLIENT, {
     variables: { id: client.id },
-    update(cache, { data: { deleteClient } }) {
-      const { clients } = cache.readQuery({ query: GET_CLIENTS });
-      cache.writeQuery({
-        query: GET_CLIENTS,
-        data: {
-          clients: clients.filter((client) => client.id !== deleteClient.id),
-        },
-      });
-    },
+    refetchQueries: [{ query: GET_CLIENTS }, { query: GET_PROJECTS }],
   });
 
   return (
